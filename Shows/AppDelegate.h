8e3f35b24d1203c656378ed1f4ff723a77606aa7
chiennguyen <chiennguyen44@yahoo.com>
2014-04-13 10:26:06 -0700

    Initial Commit

---
+++ Shows/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Shows
+//
+//  Created by CHIEN NGUYEN on 4/13/14.
+//  Copyright (c) 2014 CHIEN NGUYEN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ShowsTests/ShowsTests.m
@@ -0,0 +1,34 @@
+//
+//  ShowsTests.m
+//  ShowsTests
+//
+//  Created by CHIEN NGUYEN on 4/13/14.
+//  Copyright (c) 2014 CHIEN NGUYEN. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ShowsTests : XCTestCase
+
+@end
+
+@implementation ShowsTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
